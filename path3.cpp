fc402b15cf93ef873e894dc17a51bb3dd1a2d4e0
minsoonss <wp024302@gmail.com>
2016-08-27 18:01:33 +0900

    Update path3.cpp

--- path3.cpp
+++ path3.cpp
@@ -1,114 +1,114 @@
-//#include <cstdio>
-//#include <cstdlib>
-//#define abs(x)        ( ((x)<0)?-(x):(x) )
-//struct info{
-//	bool e,s,w,n;//µ¿,³²,¼­,ºÏ ¿ì¼±¼øÀ§°í·Á
-//	int prex,prey,min;//ÀÌÀü°æ·Î,ÃÖ¼Ò°ª
-//	int v;//°ª
-//};
-//struct pf{
-//	int x,y,v;
-//};
-//FILE *in = fopen("path3.inp","r");
-//FILE *out = fopen("path3.out","w");
-//info path[101][101];
-//pf result[1000000];
-//bool flag[101][101];
-//int d,n,m;
-//bool check;
-//int count;
-//void input();
-//void calc(int a, int b);
-//void calc2(int a, int b, int x, int y);
-//void print();
-//int main(){
-//	input();
-//	flag[0][0] = true;
-//	calc(0,0);
-//	if(check == false)fprintf(out,"Impossible!");
-//	else{
-//		fprintf(out,"(0 0) [%d]\n",path[0][0].v);
-//		print();
-//	}
-//}
-//void input(){//ÀÔ·Â¹Þ°í µ¿ ³² ¼­ ºÏÀ» °¥ ¼ö ÀÖ´ÂÁö
-//	char a;
-//	fscanf(in,"%d %d %d",&d,&n,&m);
-//	for(int i = 0; i < n; i++){
-//		fscanf(in,"\n");
-//		for(int j = 0; j < m; j++){
-//			fscanf(in,"%c",&a);
-//			path[i][j].v = a-48;
-//			if(i == 0 && j == 0);
-//			else path[i][j].min = 99999999;
-//		}
-//	}//ÀÔ·Â³¡
-//	for(int i = 0; i < n; i++){	//µ¿³²¼­ºÏ
-//		for(int j = 0; j < m; j++){
-//			if(j < m-1){//µ¿ÂÊÃ¼Å©
-//				int a = abs(path[i][j].v-path[i][j+1].v);
-//				if(a > 5)a = 10-a;
-//				if(a <= d){
-//					path[i][j].e = true;
-//					path[i][j+1].w = true;
-//				}
-//			}
-//			if(i < n-1){//³²ÂÊÃ¼Å©
-//				int a = abs(path[i][j].v-path[i+1][j].v);
-//				if(a > 5)a = 10-a;
-//				if(a <= d){
-//					path[i][j].s = true;
-//					path[i+1][j].n = true;
-//				}
-//			}
-//		}
-//	}//Ã¼Å©³¡
-//}
-//void calc(int a, int b){
-//	if(a == n-1 && b == m-1){
-//		check = true;
-//		return;
-//	}
-//	if(path[a][b].e == true && flag[a][b+1] == false && path[a][b+1].min > path[a][b].min+1 && m > b+1){//µ¿
-//		calc2(a,b,0,1);
-//		calc(a,b+1);
-//		flag[a][b+1] = false;
-//	}
-//	if(path[a][b].s == true && flag[a+1][b] == false && path[a+1][b].min > path[a][b].min+1 && n > a+1){//³²
-//		calc2(a,b,1,0);
-//		calc(a+1,b);
-//		flag[a+1][b] = false;
-//	}
-//	if(path[a][b].w == true && flag[a][b-1] == false && path[a][b-1].min > path[a][b].min+1 && 0 <= b-1){//¼­
-//		calc2(a,b,0,-1);
-//		calc(a,b-1);
-//		flag[a][b-1] = false;
-//	}
-//	if(path[a][b].n == true && flag[a-1][b] == false && path[a-1][b].min > path[a][b].min+1 && 0 <= a-1){//ºÏ
-//		calc2(a,b,-1,0);
-//		calc(a-1,b);
-//		flag[a-1][b] = false;
-//	}
-//}
-//void calc2(int a, int b, int x, int y){
-//	flag[a+x][b+y] = true;
-//	path[a+x][b+y].min = path[a][b].min+1;
-//	path[a+x][b+y].prex = a;
-//	path[a+x][b+y].prey = b;
-//}
-//void print(){
-//	int x = n-1;
-//	int y = m-1;
-//	while(true){
-//		result[count].x = x;
-//		result[count].y = y;
-//		result[count].v = path[x][y].v;
-//		x = path[result[count].x][result[count].y].prex;
-//		y = path[result[count].x][result[count].y].prey;
-//		if(x == 0 && y == 0)break;
-//		count++;
-//	}
-//	for(int i = count; i >= 0; i--){
-//		fprintf(out,"(%d %d) [%d]\n",result[i].x,result[i].y,result[i].v);
-//	}
-//}
+#include <cstdio>
+#include <cstdlib>
+#define abs(x)        ( ((x)<0)?-(x):(x) )
+struct info{
+	bool e,s,w,n;//ë™,ë‚¨,ì„œ,ë¶ ìš°ì„ ìˆœìœ„ê³ ë ¤
+	int prex,prey,min;//ì´ì „ê²½ë¡œ,ìµœì†Œê°’
+	int v;//ê°’
+};
+struct pf{
+	int x,y,v;
+};
+FILE *in = fopen("path3.inp","r");
+FILE *out = fopen("path3.out","w");
+info path[101][101];
+pf result[1000000];
+bool flag[101][101];
+int d,n,m;
+bool check;
+int count;
+void input();
+void calc(int a, int b);
+void calc2(int a, int b, int x, int y);
+void print();
+int main(){
+	input();
+	flag[0][0] = true;
+	calc(0,0);
+	if(check == false)fprintf(out,"Impossible!");
+	else{
+		fprintf(out,"(0 0) [%d]\n",path[0][0].v);
+		print();
+	}
+}
+void input(){//ìž…ë ¥ë°›ê³  ë™ ë‚¨ ì„œ ë¶ì„ ê°ˆ ìˆ˜ ìžˆëŠ”ì§€
+	char a;
+	fscanf(in,"%d %d %d",&d,&n,&m);
+	for(int i = 0; i < n; i++){
+		fscanf(in,"\n");
+		for(int j = 0; j < m; j++){
+			fscanf(in,"%c",&a);
+			path[i][j].v = a-48;
+			if(i == 0 && j == 0);
+  		else path[i][j].min = 99999999;
+		}
+	}//ìž…ë ¥ë
+	for(int i = 0; i < n; i++){	//ë™ë‚¨ì„œë¶
+		for(int j = 0; j < m; j++){
+			if(j < m-1){//ë™ìª½ì²´í¬
+				int a = abs(path[i][j].v-path[i][j+1].v);
+				if(a > 5)a = 10-a;
+				if(a <= d){
+					path[i][j].e = true;
+					path[i][j+1].w = true;
+				}
+			}
+			if(i < n-1){//ë‚¨ìª½ì²´í¬
+				int a = abs(path[i][j].v-path[i+1][j].v);
+				if(a > 5)a = 10-a;
+				if(a <= d){
+					path[i][j].s = true;
+					path[i+1][j].n = true;
+				}
+			}
+		}
+	}//ì²´í¬ë
+}
+void calc(int a, int b){
+	if(a == n-1 && b == m-1){
+		check = true;
+		return;
+	}
+	if(path[a][b].e == true && flag[a][b+1] == false && path[a][b+1].min > path[a][b].min+1 && m > b+1){//ë™
+		calc2(a,b,0,1);
+		calc(a,b+1);
+		flag[a][b+1] = false;
+	}
+	if(path[a][b].s == true && flag[a+1][b] == false && path[a+1][b].min > path[a][b].min+1 && n > a+1){//ë‚¨
+		calc2(a,b,1,0);
+		calc(a+1,b);
+		flag[a+1][b] = false;
+	}
+	if(path[a][b].w == true && flag[a][b-1] == false && path[a][b-1].min > path[a][b].min+1 && 0 <= b-1){//ì„œ
+		calc2(a,b,0,-1);
+		calc(a,b-1);
+		flag[a][b-1] = false;
+	}
+	if(path[a][b].n == true && flag[a-1][b] == false && path[a-1][b].min > path[a][b].min+1 && 0 <= a-1){//ë¶
+		calc2(a,b,-1,0);
+		calc(a-1,b);
+		flag[a-1][b] = false;
+	}
+}
+void calc2(int a, int b, int x, int y){
+	flag[a+x][b+y] = true;
+	path[a+x][b+y].min = path[a][b].min+1;
+	path[a+x][b+y].prex = a;
+	path[a+x][b+y].prey = b;
+}
+void print(){
+	int x = n-1;
+	int y = m-1;
+	while(true){
+		result[count].x = x;
+		result[count].y = y;
+		result[count].v = path[x][y].v;
+		x = path[result[count].x][result[count].y].prex;
+		y = path[result[count].x][result[count].y].prey;
+		if(x == 0 && y == 0)break;
+		count++;
+	}
+	for(int i = count; i >= 0; i--){
+		fprintf(out,"(%d %d) [%d]\n",result[i].x,result[i].y,result[i].v);
+	}
+}
